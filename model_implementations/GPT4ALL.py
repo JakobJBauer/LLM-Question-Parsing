1026b66aec98855dd4cdb07d829086fb0aa56e6b
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2023-06-08 19:49:00 +0200

    Added logging

--- model_implementations/GPT4ALL.py
+++ model_implementations/GPT4ALL.py
@@ -4,7 +4,9 @@ from gpt4all import GPT4All as __GPT4All
 
 class GPT4AllModel(AbstractModel):
     def _send_prompt(self, prompt) -> str:
+        print(f"Sending prompt ...{prompt[:-100]}")
         gptj = __GPT4All("ggml-gpt4all-j-v1.3-groovy")
         messages = [{"role": "user", "content": prompt}]
         response = gptj.chat_completion(messages)
+        print(f"Response: {response}")
         return response["message"]["content"].strip()
