e44d4b17f44ca598de4aafd8e8257f2e1626748d
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2023-06-08 22:37:11 +0200

    Adjusted for rate limits

--- model_implementations/GPT3_5Model.py
+++ model_implementations/GPT3_5Model.py
@@ -1,11 +1,13 @@
 from abstract_model import AbstractModel
 import openai
+from time import sleep
 
 openai.api_key = "<key>"
 
 
 class GPT35Model(AbstractModel):
     def _send_prompt(self, prompt) -> str:
+        sleep(2) # Rate limits by openai
         model = "gpt-3.5-turbo"
         response = openai.ChatCompletion.create(
             model=model,
