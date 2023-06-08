9ad148159e625f1cb30c09e913aab0ecf568b8b2
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2023-06-08 19:57:53 +0200

    Fixed import issues

--- model_implementations/GPT4AllModel.py
+++ model_implementations/GPT4AllModel.py
@@ -1,11 +1,11 @@
 from abstract_model import AbstractModel
-from gpt4all import GPT4All as __GPT4All
+from gpt4all import GPT4All as GPT4All
 
 
 class GPT4AllModel(AbstractModel):
     def _send_prompt(self, prompt) -> str:
-        print(f"Sending prompt ...{prompt[:-100]}")
-        gptj = __GPT4All("ggml-gpt4all-j-v1.3-groovy")
+        print(f"Sending prompt ...{prompt[-100:]}")
+        gptj = GPT4All("ggml-gpt4all-j-v1.3-groovy")
         messages = [{"role": "user", "content": prompt}]
         response = gptj.chat_completion(messages)
         print(f"Response: {response}")
--- run_gpt4all.py
+++ run_gpt4all.py
@@ -1,4 +1,4 @@
-from model_implementations.GPT4ALL import GPT4AllModel
+from model_implementations.GPT4AllModel import GPT4AllModel
 
 m = GPT4AllModel("GPT4ALL")
 m.run()
