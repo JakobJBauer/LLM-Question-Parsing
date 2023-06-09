bdd505790411d65169436676fce37c9ec7fe0cdd
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2023-06-09 16:27:36 +0200

    Added Vicuna 13b support

---
+++ model_implementations/Vicuna13bModel.py
@@ -0,0 +1,14 @@
+from abstract_model import AbstractModel
+import replicate
+
+
+class Vicuna13bModel(AbstractModel):
+    def _send_prompt(self, prompt) -> str:
+        response = replicate.run(
+            "replicate/vicuna-13b:6282abe6a492de4145d7bb601023762212f9ddbbe78278bd6771c8b3b2f2a13b",
+            input={
+                "prompt": prompt,
+                "max_length": 5000
+            }
+        )
+        return " ".join(list(response))
---
+++ run_vicuna13b.py
@@ -0,0 +1,5 @@
+from model_implementations.Vicuna13bModel import Vicuna13bModel
+
+m = Vicuna13bModel("Vicuna 13b")
+m.run()
+m.close_logfile()
