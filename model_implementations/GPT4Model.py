1c653661801b259594d242db9f153a5b45510239
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2023-06-09 05:10:48 +0200

    Added logging for gpt 4

--- model_implementations/GPT4Model.py
+++ model_implementations/GPT4Model.py
@@ -14,12 +14,15 @@ class GPT4Model(AbstractModel):
 
     def _send_prompt(self, prompt) -> str:
         if self.__prompt_counter == 25:
+            print(f"Limit reached, will wait {3*60*60 - self.__time_waited} seconds")
             self.__prompt_counter = 0
             sleep(3*60*60 - self.__time_waited)
             self.__time_waited = 0
         time_to_wait = random.randint(15, 3 * 60 * 60 // 35)
         self.__time_waited += time_to_wait
         response = self.__model(prompt)
+        print(f"Response: {response}")
+        print(f"Time to wait: {time_to_wait} seconds")
         sleep(time_to_wait)
         self.__prompt_counter += 1
         return response
